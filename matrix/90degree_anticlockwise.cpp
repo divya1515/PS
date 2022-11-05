495c3768e32f32095389d8cad9cc258b1b8f9b87
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-11-06 03:04:06 +0530

    Create 90degree_anticlockwise.cpp

---
+++ matrix/90degree_anticlockwise.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int nums[n][n];
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n;j++)
+    {
+        cin>>nums[i][j];
+    }
+  }
+  for(int i=0;i<n;i++)
+  {
+      for(int j=0;j<=i;j++)
+    {
+       swap(nums[i][j],nums[j][i]);
+    }
+  }
+  for(int j=0;j<n;j++)
+  {
+    for(int i=0;i<n/2;i++)
+    {
+      swap(nums[i][j],nums[n-i-1][j]);
+    }
+  }
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n;j++)
+    {
+        cout<<nums[i][j]<<" ";
+    }
+    cout<<"\n";
+  }
+
+}
