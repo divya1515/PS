d8501d1e15242f018e028f0d1e6e4743d6326edb
divya1515 <114672831+divya1515@users.noreply.github.com>
2022-11-06 12:39:52 +0530

    Create 180_degree_anticlockwise.cpp

---
+++ matrix/180_degree_anticlockwise.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  int nums[n][n];
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      cin >> nums[i][j];
+    }
+  }
+  for (int j = 0; j < n; j++)
+  {
+    for (int i = 0; i < n / 2; i++)
+    {
+      swap(nums[i][j], nums[n - i - 1][j]);
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n / 2; j++)
+    {
+      swap(nums[i][j], nums[i][n - j - 1]);
+    }
+  }
+  
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      cout << nums[i][j] << " ";
+    }
+    cout << "\n";
+  }
+}
